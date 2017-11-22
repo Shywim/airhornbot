fe8669f7caec544ee93a189b27b3bc59dcd5e041
Matthieu Harlé <hello@matthieuharle.com>
2017-11-22 15:46:11 +0100

    Cleanup post templates

--- web-app/src/EventSource.d.ts
+++
@@ -1,7 +0,0 @@
-interface Callback { (data: any): void; }
-
-declare class EventSource {
-    onmessage: Callback;
-    addEventListener(event: string, cb: Callback): void;
-    constructor(name: string);
-}
